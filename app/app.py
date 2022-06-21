59ec4b60d2fa1fffc0664842642818f8fb4f0a58
JohnnySD <johnsdieckmann@gmail.com>
2022-06-21 17:14:00 +1000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
+from getpass import getuser
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 	return render_template('index.html')
+
+@app.route('/hello')
+def hello():
+	return render_template('hello.html')
+
+@app.route('/hello/<name>/')
+def helloName(name):
+	return f'Hello, <code>{name}</code>!'
 
 if __name__ == '__main__':
 	app.run()
